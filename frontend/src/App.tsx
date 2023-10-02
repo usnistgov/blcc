fda887127981bd78d8fba2b9469218d27ae59a2d
Luke Donmoyer <luke.donmoyer@nist.gov>
2023-10-02 11:57:19 -0400

    Framework for some base components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,15 @@
-import button from "./components/Button";
-import {useSubscribe} from "./hooks/UseSubscribe";
-import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
-import useModelParam from "./hooks/UseModelParam";
-import Model from "./model/Model";
-
-const {click$: helloClick$, component: HelloButton} = button();
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import EditorAppBar from "./components/EditorAppBar";
+import ResultsAppBar from "./components/ResultsAppBar";
 
 export default function App() {
-    useSubscribe(Model.signal$, (value) => console.log(value));
-
     return <BrowserRouter>
         <Routes>
-            <Route path={"/"} element={<Root/>}/>
-            <Route path={":test"} element={<Test/>}/>
+            <Route path={"/results/*"} element={<ResultsAppBar/>}/>
+            <Route path={"/*"} element={<EditorAppBar/>}/>
+        </Routes>
+        <Routes>
+
         </Routes>
     </BrowserRouter>
-}
-
-function Root() {
-    const navigate = useNavigate();
-    useSubscribe(helloClick$, () => navigate("/asdf"));
-
-    return <HelloButton>
-        Hello, World!
-    </HelloButton>
-}
-
-function Test() {
-    useModelParam("test", Model.setSignal);
-    return <div>
-        Hello, World!
-    </div>
 }
---
+++ frontend/src/components/ResultsAppBar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ButtonBar from "./ButtonBar";
+import button, {ButtonType} from "./Button";
+import AppBar from "./AppBar";
+import {mdiArrowLeft, mdiPlay} from "@mdi/js";
+import {useNavigate} from "react-router-dom";
+import {useSubscribe} from "../hooks/UseSubscribe";
+import HelpButtons from "./HelpButtons";
+import Model from "../model/Model";
+
+const {click$: backClick$, component: BackButton} = button();
+const {click$: runClick$, component: RunButton} = button();
+
+export default function ResultsAppBar() {
+    const navigate = useNavigate();
+    const projectName = Model.useProjectName();
+
+    useSubscribe(backClick$, () => navigate(-1), [navigate]);
+
+    return <AppBar>
+        <ButtonBar className={"p-2"}>
+            <BackButton type={ButtonType.PRIMARY} icon={mdiArrowLeft} iconSide={"left"}>
+                Back to Editor
+            </BackButton>
+        </ButtonBar>
+        <div className={"flex flex-row place-items-center gap-4 divide-x-2 divide-white"}>
+            <p className={"text-white"}>{projectName}</p>
+            <div className={"pl-4"}>
+                <RunButton type={ButtonType.PRIMARY} inverted icon={mdiPlay}>
+                    Run
+                </RunButton>
+            </div>
+        </div>
+        <HelpButtons/>
+    </AppBar>
+}
