222ac8462cc12df26a3a0c7b27a87e27b6dd2860
purav-parekh <purav98@gmail.com>
2023-10-03 16:09:41 -0400

    add textinput component

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,45 +1,52 @@
-import {Observable} from "rxjs";
-import React, {PropsWithChildren} from "react";
-import {createSignal} from "@react-rxjs/utils";
-import Icon from "@mdi/react";
+import { Observable } from "rxjs";
+import React, { PropsWithChildren } from "react";
+import { createSignal } from "@react-rxjs/utils";
+import { Input } from "antd";
 
 export enum TextInputType {
-    PRIMARY = "w-44",
-    PRIMARY_INVERTED = " bg-base-lightest hover:bg-base-lighter active:bg-base-light text-primary ",
-    ERROR = " bg-error hover:bg-error-light active:bg-error-dark text-base-lightest ",
-    SUCCESS = " bg-success hover:bg-success-light active:bg-success-dark text-base-lightest ",
-    DISABLED = " bg-base-lighter text-base-light "
+    PRIMARY = "border-solid border-2 border-black hover:border-black active:border-black",
+    ERROR = "border-solid border-2 border-red-600 active:border-red-600 hover:border-red-600",
+    SUCCESS = "border-solid border-2 border-green-600 active:border-green-600 hover:border-green-600",
+    DISABLED = "bg-base-lighter text-base-light"
 }
-
 
 export type TextInputProps = {
     className?: string;
     type: TextInputType;
-    icon?: string;
-    disabled?: boolean
-}
+    disabled?: boolean;
+    placeholder?: string;
+    bordered?: boolean;
+};
 
 export type TextInput = {
     onChange$: Observable<void>;
     component: (props: PropsWithChildren & TextInputProps) => React.JSX.Element;
-}
+};
 
 export default function textInput(): TextInput {
-    const [onChange$, onChange] = createSignal()
+    const [onChange$, onChange] = createSignal();
 
     return {
         onChange$,
         component: ({
-                children,
-                className,
-                type,
-                icon,
-                disabled = false,
-            }: PropsWithChildren & TextInputProps) => {
-                return <input className={(className ?? "") + ` ${disabled ? TextInputType.DISABLED : type}`} 
-                onChange={onChange}>
+            children,
+            className,
+            type,
+            disabled = false,
+            bordered = true,
+            placeholder
+        }: PropsWithChildren & TextInputProps) => {
+            return (
+                <Input
+                    className={(className ?? "") + `${disabled ? TextInputType.DISABLED : type} w-44`}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    bordered={bordered}
+                    disabled={disabled}
+                >
                     {children}
-                </input>
-            }
+                </Input>
+            );
         }
+    };
 }
