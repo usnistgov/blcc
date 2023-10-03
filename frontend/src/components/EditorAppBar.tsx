841d24a5f0140d42ca15755f9d0a4b4c466e84f0
purav-parekh <purav98@gmail.com>
2023-10-03 15:02:04 -0400

    add prettier,eslint & format files

--- frontend/src/components/EditorAppBar.tsx
+++ frontend/src/components/EditorAppBar.tsx
@@ -1,18 +1,18 @@
 import ButtonBar from "./ButtonBar";
-import button, {ButtonType} from "./Button";
-import {mdiContentSave, mdiFileDocumentPlus, mdiFolder, mdiPlay} from "@mdi/js";
+import button, { ButtonType } from "./Button";
+import { mdiContentSave, mdiFileDocumentPlus, mdiFolder, mdiPlay } from "@mdi/js";
 import AppBar from "./AppBar";
 import Model from "../model/Model";
-import {useNavigate} from "react-router-dom";
-import {useSubscribe} from "../hooks/UseSubscribe";
+import { useNavigate } from "react-router-dom";
+import { useSubscribe } from "../hooks/UseSubscribe";
 import HelpButtons from "./HelpButtons";
 
-const {component: NewButton} = button();
-const {component: OpenButton} = button();
-const {component: SaveButton} = button();
-const {component: SaveAsButton} = button();
+const { component: NewButton } = button();
+const { component: OpenButton } = button();
+const { component: SaveButton } = button();
+const { component: SaveAsButton } = button();
 
-const {click$: runAnalysisClick$, component: RunAnalysisButton} = button();
+const { click$: runAnalysisClick$, component: RunAnalysisButton } = button();
 
 /**
  * The app bar for the editor context.
@@ -23,29 +23,31 @@ export default function EditorAppBar() {
 
     useSubscribe(runAnalysisClick$, () => navigate("/results"), [navigate]);
 
-    return <AppBar className={"bg-primary"}>
-        <ButtonBar className={"p-2"}>
-            <NewButton type={ButtonType.PRIMARY} icon={mdiFileDocumentPlus}>
-                New
-            </NewButton>
-            <OpenButton type={ButtonType.PRIMARY} icon={mdiFolder}>
-                Open
-            </OpenButton>
-            <SaveButton type={ButtonType.PRIMARY} icon={mdiContentSave}>
-                Save
-            </SaveButton>
-            <SaveAsButton type={ButtonType.PRIMARY} icon={mdiContentSave}>
-                Save As
-            </SaveAsButton>
-        </ButtonBar>
-        <div className={"flex flex-row place-items-center gap-4 divide-x-2 divide-white"}>
-            <p className={"text-base-lightest"}>{projectName}</p>
-            <div className={"pl-4"}>
-                <RunAnalysisButton type={ButtonType.PRIMARY_INVERTED} icon={mdiPlay}>
-                    Reports and Analysis
-                </RunAnalysisButton>
+    return (
+        <AppBar className={"bg-primary"}>
+            <ButtonBar className={"p-2"}>
+                <NewButton type={ButtonType.PRIMARY} icon={mdiFileDocumentPlus}>
+                    New
+                </NewButton>
+                <OpenButton type={ButtonType.PRIMARY} icon={mdiFolder}>
+                    Open
+                </OpenButton>
+                <SaveButton type={ButtonType.PRIMARY} icon={mdiContentSave}>
+                    Save
+                </SaveButton>
+                <SaveAsButton type={ButtonType.PRIMARY} icon={mdiContentSave}>
+                    Save As
+                </SaveAsButton>
+            </ButtonBar>
+            <div className={"flex flex-row place-items-center gap-4 divide-x-2 divide-white"}>
+                <p className={"text-base-lightest"}>{projectName}</p>
+                <div className={"pl-4"}>
+                    <RunAnalysisButton type={ButtonType.PRIMARY_INVERTED} icon={mdiPlay}>
+                        Reports and Analysis
+                    </RunAnalysisButton>
+                </div>
             </div>
-        </div>
-        <HelpButtons/>
-    </AppBar>
+            <HelpButtons />
+        </AppBar>
+    );
 }
--- frontend/src/pages/results/AnnualResults.tsx
+++ frontend/src/pages/results/AnnualResults.tsx
@@ -1,5 +1,7 @@
 export default function AnnualResults() {
-    return <div className={"w-full h-full bg-base"}>
-        <h1>Annual Results</h1>
-    </div>
+    return (
+        <div className={"w-full h-full bg-base"}>
+            <h1>Annual Results</h1>
+        </div>
+    );
 }
