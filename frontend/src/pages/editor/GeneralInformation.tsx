e544f094f28c068bb4f998680bc94ac0e67d4c4d
purav-parekh <purav98@gmail.com>
2023-10-16 11:44:00 -0400

    add modularity

--- frontend/src/pages/editor/GeneralInformation.tsx
+++ frontend/src/pages/editor/GeneralInformation.tsx
@@ -1,8 +1,10 @@
-import { Typography, Divider } from "antd";
+import { Typography, Divider, Switch } from "antd";
 
 import textInput, { TextInputType } from "../../components/TextInput";
 import textArea from "../../components/TextArea";
 import dropdown from "../../components/Dropdown";
+
+import { analysisType, analysisPurpose, emissionsRateScenario, socialCostOfGHG } from "../../constants/DROPDOWN";
 
 const { component: TextInput } = textInput();
 const { component: TextArea } = textArea();
@@ -11,116 +13,101 @@ const { component: DropDown } = dropdown();
 const { Title } = Typography;
 
 export default function GeneralInformation() {
-    const analysisType: string[] = [
-        "Federal Analysis, Financed Project",
-        "FEMP Analysis, Energy Project",
-        "OMB Analysis, Non-Energy Project",
-        "MILCON Analysis, Energy Project",
-        "MILCON Analysis, Non-Energy Project",
-        "MILCON Analysis, ECIP Project",
-        "FEMP Analysis, ESPC Project "
-    ];
-
-    const analysisPurpose: string[] = [
-        "Cost-effectiveness- lease-purchase- internal government investment & asset sales",
-        "Public Investment & Regulatory Analyses"
-    ];
-
     const studyPeriod: string[] = [];
     for (let i = 0; i < 41; i++) studyPeriod.push(i + " years");
 
     return (
-        <div className={"w-full h-full bg-base p-8"}>
-            <div className="w-1/2">
-                <div className="flex">
-                    <span className="">
-                        <Title level={5}>Project Name</Title>
-                        <TextInput type={TextInputType.PRIMARY} />
-                    </span>
-                    <span>
-                        <Title level={5}>Analyst</Title>
-                        <TextInput type={TextInputType.PRIMARY} />
-                    </span>
-                </div>
-                <div className="flex ">
-                    <span className="">
-                        <Title level={5}>Analysis Type</Title>
-                        <DropDown className="" options={analysisType} />
-                    </span>
-                    <span className="">
-                        <Title level={5}>Analysis Purpose</Title>
-                        <DropDown options={analysisPurpose} />
-                    </span>
-                </div>
-                <span className="">
+        <div className={"w-full h-full bg-base p-8 "}>
+            <div className="w-1/2 grid grid-cols-2">
+                <span className="pb-3">
+                    <Title level={5}>Project Name</Title>
+                    <TextInput type={TextInputType.PRIMARY} />
+                </span>
+                <span>
+                    <Title level={5}>Analyst</Title>
+                    <TextInput type={TextInputType.PRIMARY} />
+                </span>
+
+                <span className="pb-3">
+                    <Title level={5}>Analysis Type</Title>
+                    <DropDown className="w-3/4" options={analysisType} />
+                </span>
+                <span className="pb-3">
+                    <Title level={5}>Analysis Purpose</Title>
+                    <DropDown className="w-3/4" options={analysisPurpose} />
+                </span>
+
+                <span className="col-span-2 pb-3">
                     <Title level={5}>Description</Title>
                     <TextArea className="w-full" />
                 </span>
-                <span className="">
+                <span className="pb-3">
                     <Title level={5}>Length of Study Period</Title>
-                    <DropDown options={studyPeriod} />
+                    <DropDown className="w-1/2" options={studyPeriod} />
                 </span>
             </div>
+            <span className="w-1/4 pb-3">
+                <Title level={5}>Dollar Analysis</Title>
+                <Switch className="" checkedChildren="Consant" unCheckedChildren="Current" defaultChecked />
+            </span>
+            <div className="grid grid-cols-2">
+                <div className="grid grid-cols-2">
+                    <Divider className="col-span-2 pb-3" orientation="left" orientationMargin="0">
+                        Discounting
+                    </Divider>
+                    <span className="pb-3">
+                        <Title level={5}>Discounting Convention</Title>
+                        <DropDown className="w-1/2" options={["End of Year", "Mid Year"]} />
+                    </span>
+                    <span className="pb-3">
+                        <Title level={5}>General Inflation Rate</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                    <span className="pb-3 w-full">
+                        <Title level={5}>Nominal Discount Rate</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                    <span>
+                        <Title level={5}>Real Discount Rate</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                </div>
+                <div className="grid grid-cols-2">
+                    <Divider className="col-span-2 pb-3" orientation="left" orientationMargin="0">
+                        Location
+                    </Divider>
+                    <span className="pb-3">
+                        <Title level={5}>Country</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                    <span>
+                        <Title level={5}>City</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                    <span className="pb-3">
+                        <Title level={5}>State</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                    <span>
+                        <Title level={5}>Zip</Title>
+                        <TextInput type={TextInputType.PRIMARY} />
+                    </span>
+                </div>
+            </div>
 
-            <div className="flex">
-                <div className="w-1/2">
-                    <div className="">
-                        <Divider orientation="left" orientationMargin="0">
-                            Discounting
-                        </Divider>
-                        <div className="flex">
-                            <span>
-                                <Title level={5}>Discounting Convention</Title>
-                                <DropDown options={["End of Year", "Mid Year"]} />
-                            </span>
-                            <span className="">
-                                <Title level={5}>General Inflation Rate</Title>
-                                <TextInput type={TextInputType.PRIMARY} />
-                            </span>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="flex">
-                            <span className="">
-                                <Title level={5}>Nominal Discount Rate</Title>
-                                <TextInput type={TextInputType.PRIMARY} />
-                            </span>
-                            <span>
-                                <Title level={5}>Real Discount Rate</Title>
-                                <TextInput type={TextInputType.PRIMARY} />
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-1/2">
-                    <div className="">
-                        <Divider orientation="left" orientationMargin="0">
-                            Location
-                        </Divider>
-                        <div className="flex">
-                            <span className="">
-                                <Title level={5}>Country</Title>
-                                <TextInput type={TextInputType.PRIMARY} />
-                            </span>
-                            <span>
-                                <Title level={5}>City</Title>
-                                <TextInput type={TextInputType.PRIMARY} />
-                            </span>
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <span className="">
-                            <Title level={5}>State</Title>
-                            <TextInput type={TextInputType.PRIMARY} />
-                        </span>
-                        <span>
-                            <Title level={5}>Zip</Title>
-                            <TextInput type={TextInputType.PRIMARY} />
-                        </span>
-                    </div>
-                </div>
+            <div className="grid grid-cols-2">
+                <Divider className="col-span-2 pb-3" orientation="left" orientationMargin="0">
+                    Greenhouse Gas (GHG) Emissions and Cost Assumptions
+                </Divider>
+                <span className="pb-3">
+                    <Title level={5}>Emissions Rate Scenario</Title>
+                    <DropDown className="w-1/2" options={emissionsRateScenario} />
+                </span>
+                <span className="pb-3">
+                    <Title level={5}>Social Cost of GHG Scenario </Title>
+                    <DropDown className="w-1/2" options={socialCostOfGHG} />
+                </span>
             </div>
         </div>
     );
 }
-
