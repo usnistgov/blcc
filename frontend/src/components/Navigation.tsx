5b1cea922ee7a6378f26334ff4b8931909c2b197
purav-parekh <purav98@gmail.com>
2023-10-18 11:29:30 -0400

    design sidebar

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { DesktopOutlined, PieChartOutlined, UserOutlined } from "@ant-design/icons";
+import { Layout, Menu, type MenuProps } from "antd";
 
 const { Sider } = Layout;
 
@@ -26,41 +17,35 @@ function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode,
 }
 
 const items: MenuItem[] = [
-    getItem("General Information", "1", <PieChartOutlined />),
-    getItem("Alternative Summary", "2", <DesktopOutlined />),
-    getItem("Alternatives", "sub1", <UserOutlined />, [getItem("Tom", "3"), getItem("Bill", "4"), getItem("Alex", "5")])
+    getItem("General Information", "gen-info", <PieChartOutlined />),
+    getItem("Alternative Summary", "alt-sum", <DesktopOutlined />),
+    getItem("Alternatives", "alt", <UserOutlined />, [
+        getItem("Alternatives 1", "alt-1"),
+        getItem("Alternatives 2", "alt-2"),
+        getItem("Alternatives 3", "alt-3")
+    ])
 ];
-
-const items2: MenuProps["items"] = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-    const key = String(index + 1);
-
-    return {
-        key: `sub${key}`,
-        icon: React.createElement(icon),
-        label: `subnav ${key}`,
-
-        children: new Array(4).fill(null).map((_, j) => {
-            const subKey = index * 4 + j + 1;
-            return {
-                key: subKey,
-                label: `option${subKey}`
-            };
-        })
-    };
-});
 
 export default function Navigation() {
     const [collapsed, setCollapsed] = useState(false);
-    const {
-        token: { colorBgContainer }
-    } = theme.useToken();
     return (
         <>
-            {/* <div className={"w-fit h-full bg-base-light"}>Navigation</div> */}
-            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                <div className="demo-logo-vertical" />
-                <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={items} />
-            </Sider>
+            <Layout style={{ minHeight: "100vh", background: "rgb(0 94 162)" }}>
+                <Sider
+                    collapsible
+                    collapsed={collapsed}
+                    onCollapse={(value) => setCollapsed(value)}
+                    style={{ background: "rgb(0 94 162)" }}
+                >
+                    <div className="demo-logo-vertical" style={{ background: "rgb(0 94 162)" }} />
+                    <Menu
+                        style={{ background: "rgb(0 94 162)", color: "#fff" }}
+                        defaultSelectedKeys={["gen-info"]}
+                        mode="inline"
+                        items={items}
+                    />
+                </Sider>
+            </Layout>
             <Outlet />
         </>
     );
