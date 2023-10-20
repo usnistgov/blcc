e879734f2d2667969cb9894b09bdf9ffc3f89913
Luke Donmoyer <luke.donmoyer@nist.gov>
2023-10-20 12:34:13 -0400

    Added hook creation function

--- frontend/src/components/Statistics.tsx
+++ frontend/src/components/Statistics.tsx
@@ -1,13 +1,20 @@
 import AppBar from "./AppBar";
 import Icon from "@mdi/react";
 import { mdiCheck } from "@mdi/js";
+import { modelHooks } from "../model/Model";
+import { Alternative, Cost } from "../blcc-format/Format";
+
+const { useAlternatives, useCosts } = modelHooks({
+    alternatives: [] as Alternative[],
+    costs: [] as Cost[]
+});
 
 export default function Statistics() {
     return (
         <AppBar className={"bg-base-lightest text-sm place-items-center"}>
             <div className={"flex flex-row divide-x divide-base-light border-t"}>
-                <p className={"px-4"}>Alternatives 1</p>
-                <p className={"px-4"}>Costs 3</p>
+                <p className={"px-4"}>{`Alternatives ${useAlternatives()?.length}`}</p>
+                <p className={"px-4"}>{`Costs ${useCosts()?.length}`}</p>
             </div>
             <div>
                 <Icon className={"mx-2 text-success"} path={mdiCheck} size={0.8} />
