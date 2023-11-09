d991d378dcc6e11d5a6c414c23f379b69571e29d
purav-parekh <purav98@gmail.com>
2023-11-09 11:24:32 -0500

    combined stream from multiple streams for costs

--- frontend/src/pages/editor/AlternativeSummary.tsx
+++ frontend/src/pages/editor/AlternativeSummary.tsx
@@ -3,26 +3,41 @@ import { Divider, Typography } from "antd";
 import button, { ButtonType } from "../../components/Button";
 import Icon from "@mdi/react";
 import { mdiPlus } from "@mdi/js";
+import { mergeMap, from } from "rxjs";
+import { bind } from "@react-rxjs/core";
+import { map, toArray, withLatestFrom } from "rxjs/operators";
 
 const { Title } = Typography;
 const { component: Button } = button();
 
+const combinedCostObject$ = Model.costs$.pipe(
+    map((costs) => {
+        const costObject = costs.reduce((result, cost) => {
+            result[cost.id] = cost;
+            return result;
+        }, {});
+        return costObject;
+    })
+);
+
+const altCostObject$ = Model.alternatives$.pipe(
+    withLatestFrom(combinedCostObject$),
+    mergeMap(([altArray, combinedCostObj]) =>
+        from(altArray).pipe(
+            map((alt) => {
+                const updatedCosts = alt.costs.map((cost) => combinedCostObj[cost]);
+                return { ...alt, costs: updatedCosts };
+            }),
+            toArray()
+        )
+    )
+);
+
+const [useAltCostObject] = bind(altCostObject$, []);
+
 export default function AlternativeSummary() {
-    const alternatives = Model.useAlternatives();
-    const costs = Model.useCosts();
-    const costsHash = {};
-    costs.forEach((item) => {
-        costsHash[item.id] = item;
-    });
+    const finalAlternatives = useAltCostObject();
 
-    alternatives.forEach((alt) => {
-        const altCosts = alt.costs.map((altCost) => {
-            return costsHash[altCost];
-        });
-        alt.costs = altCosts;
-    });
-
-    console.log(alternatives);
     return (
         <div>
             <div className={"w-screen h-full "}>
@@ -33,7 +48,7 @@ export default function AlternativeSummary() {
                     </Button>
                     <Divider className="p-0 m-0" />
                 </div>
-                {alternatives?.map((alt) => (
+                {finalAlternatives?.map((alt) => (
                     <div className="flex justify-center align-middle">
                         <div className="bg-primary-light p-5 w-1/2 rounded mb-5">
                             <Title level={4}>{alt?.name}</Title>
@@ -41,25 +56,25 @@ export default function AlternativeSummary() {
                             <br />
                             <div className="costs flex justify-between">
                                 <div className="water-costs border">
-                                    <Title level={5}>Energy Costs total</Title>
+                                    <Title level={5}>Energy Costs </Title>
                                     <Divider className="m-0" />
                                 </div>
                                 <div className="water-costs border">
-                                    <Title level={5}>Water Costs total</Title>
+                                    <Title level={5}>Water Costs </Title>
                                     <Divider className="m-0" />
                                 </div>
                                 <div className="water-costs border">
-                                    <Title level={5}>Capital Costs total</Title>
+                                    <Title level={5}>Capital Costs </Title>
                                     <Divider className="m-0" />
                                 </div>
                                 <div className="water-costs border">
-                                    <Title level={5}>Other Costs total</Title>
+                                    <Title level={5}>Other Costs </Title>
                                     <Divider className="m-0" />
                                 </div>
                             </div>
-                            {/* {alt.costs.map((cost) => (
-                                <div>{cost}</div>
-                            ))} */}
+                            {alt.costs.map((cost) => (
+                                <div>{cost?.name}</div>
+                            ))}
                         </div>
                     </div>
                 ))}
