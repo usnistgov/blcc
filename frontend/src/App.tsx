5327920e3b024b3f06a75a0c9347fe552ba8870f
Luke Donmoyer <lukedonmoyer@gmail.com>
2023-11-09 09:21:06 -0500

    Added log in App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,73 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EditorAppBar from "./components/EditorAppBar";
+import ResultsAppBar from "./components/ResultsAppBar";
+import Navigation from "./components/Navigation";
+import GeneralInformation from "./pages/editor/GeneralInformation";
+import Alternatives from "./pages/editor/Alternatives";
+import React from "react";
+import Cost from "./pages/editor/Cost";
+import AlternativeSummary from "./pages/editor/AlternativeSummary";
+import Statistics from "./components/Statistics";
+import CostNavigation from "./components/CostNavigation";
+import ResultNavigation from "./components/ResultNavigation";
+import Inputs from "./pages/results/Inputs";
+import Summary from "./pages/results/Summary";
+import AlternativeResults from "./pages/results/AlternativeResults";
+import AnnualResults from "./pages/results/AnnualResults";
+import { Subscribe } from "@react-rxjs/core";
+import { project$ } from "./model/Project";
+import { toE3Object } from "./model/E3Request";
+import { Model } from "./model/Model";
+
+//FIXME: needed to force load the project stream
+project$.subscribe(console.log);
+
+Model.project$.pipe(toE3Object()).subscribe((b) => console.log(b.build()));
+
+export default function App() {
+    return (
+        <Subscribe>
+            <BrowserRouter>
+                <div className={"flex flex-col h-full"}>
+                    {/* App bars */}
+                    <Routes>
+                        <Route path={"/editor/*"} element={<EditorAppBar />} />
+                        <Route path={"/results/*"} element={<ResultsAppBar />} />
+                    </Routes>
+
+                    <div className={"flex h-full"}>
+                        {/* Navigation */}
+                        <Routes>
+                            <Route path={"/editor"} element={<Navigation />}>
+                                <Route path={"alternative/*"} element={<CostNavigation />} />
+                            </Route>
+                            <Route path={"/results"} element={<ResultNavigation />} />
+                        </Routes>
+
+                        {/* Pages */}
+                        <Routes>
+                            <Route path={"/editor"}>
+                                <Route index element={<GeneralInformation />} />
+                                <Route path={"alternative"}>
+                                    <Route index element={<AlternativeSummary />} />
+                                    <Route path={":alternativeID"} element={<Alternatives />} />
+                                    <Route path={"cost/:costID"} element={<Cost />} />
+                                </Route>
+                            </Route>
+                            <Route path={"/results"}>
+                                <Route index element={<Inputs />} />
+                                <Route path={"alternative"} element={<AlternativeResults />} />
+                                <Route path={"annual"} element={<AnnualResults />} />
+                                <Route path={"summary"} element={<Summary />} />
+                            </Route>
+                        </Routes>
+                    </div>
+
+                    <Routes>
+                        <Route path={"/editor/*"} element={<Statistics />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Subscribe>
+    );
+}
