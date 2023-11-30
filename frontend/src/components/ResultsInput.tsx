a4483a3b0e37894c7f5abb2c2235aab2ce76d9e0
purav-parekh <purav98@gmail.com>
2023-11-30 12:33:19 -0500

    change to simple react comp

--- frontend/src/components/ResultsInput.tsx
+++ frontend/src/components/ResultsInput.tsx
@@ -7,24 +7,21 @@ import React, { PropsWithChildren } from "react";
 const { Title } = Typography;
 
 export type InputProps = {
-    className?: string;
     label: string;
-    dataVal: string | number;
+    dataVal: string | number | undefined;
 };
 
 export type ResultsInput = {
     component: React.FC<PropsWithChildren & InputProps>;
 };
 
-export default function resultsInput() {
-    return {
-        component: ({ className = "", label = "", dataVal = "NA" }: PropsWithChildren & InputProps) => {
-            return (
-                <div className={className + "pb-3"}>
-                    <Title level={5}>{label}</Title>
-                    {dataVal === "NA" ? <h2 className="text-base-light">NA</h2> : <h2>{dataVal}</h2>}
-                </div>
-            );
-        }
-    };
+export default function ResultsInput(props: InputProps) {
+    const { label, dataVal = "NA" } = props;
+
+    return (
+        <div className="pb-3">
+            <Title level={5}>{label}</Title>
+            {dataVal === "NA" ? <h2 className="text-base-light">NA</h2> : <h2>{dataVal}</h2>}
+        </div>
+    );
 }
