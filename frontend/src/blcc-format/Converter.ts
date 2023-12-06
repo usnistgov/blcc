41bb7fd8abaaecd54b1609eca116925b1d46247e
Luke Donmoyer <luke.donmoyer@nist.gov>
2023-12-06 15:33:10 -0500

    Default to FEMP_ENERGY analysis type

--- frontend/src/blcc-format/Converter.ts
+++ frontend/src/blcc-format/Converter.ts
@@ -113,7 +113,8 @@ function convertAnalysisType(old: number) {
             return AnalysisType.OMB_NON_ENERGY;
         case 5:
             return AnalysisType.MILCON_NON_ENERGY;
-        //TODO: handle possible error
+        default:
+            return AnalysisType.FEMP_ENERGY;
     }
 }
 
