5be7a81422bee1e912b731719f60cf6e4e673867
purav-parekh <purav98@gmail.com>
2023-12-13 16:14:28 -0500

    create columns & get alt ids

--- frontend/src/pages/results/AnnualResults.tsx
+++ frontend/src/pages/results/AnnualResults.tsx
@@ -1,5 +1,7 @@
+import { bind } from "@react-rxjs/core";
 import { Divider, Typography } from "antd";
 import { of } from "rxjs";
+import { map } from "rxjs/operators";
 import { json } from "../../../../docs/FederalFinancedE3Result";
 import dropdown from "../../components/Dropdown";
 import table from "../../components/Table";
@@ -10,32 +12,8 @@ const { Title } = Typography;
 
 const alts = ["Alt 1", "Alt 2", "Alt 3"];
 
-const { change$: AlternativeChange$, component: AlternativeDropdown } = dropdown(Object.values(alts));
-
 const { component: NPVComparisonTable } = table();
 const { component: NPVByAltTable } = table();
-const addlCols = alts.map((alt) => {
-    return {
-        title: alt,
-        dataIndex: alt,
-        key: alt,
-        editable: false
-    };
-    // return alt;
-});
-
-console.log(addlCols);
-
-const NPVComparisonTableCols = [
-    {
-        title: "Year",
-        dataIndex: "year",
-        key: "year",
-        editable: false
-    },
-    ...addlCols
-];
-
 const NPVByAltTableCols = [
     {
         title: "Year",
@@ -51,19 +29,19 @@ const NPVByAltTableCols = [
     },
     {
         title: "Energy",
-        dataIndex: "investment",
-        key: "investment",
+        dataIndex: "energy",
+        key: "energy",
         editable: false,
         children: [
-            { title: "Consumption", dataIndex: "use", key: "use", editable: false },
-            { title: "Demands", dataIndex: "disposal", key: "disposal", editable: false },
-            { title: "Rebates", dataIndex: "disposal", key: "disposal", editable: false }
+            { title: "Consumption", dataIndex: "consumption", key: "consumption", editable: false },
+            { title: "Demands", dataIndex: "demands", key: "demands", editable: false },
+            { title: "Rebates", dataIndex: "rebates", key: "rebates", editable: false }
         ]
     },
     {
         title: "Water",
-        dataIndex: "investment",
-        key: "investment",
+        dataIndex: "water",
+        key: "water",
         editable: false,
         children: [
             { title: "Use", dataIndex: "use", key: "use", editable: false },
@@ -72,24 +50,24 @@ const NPVByAltTableCols = [
     },
     {
         title: "OMR",
-        dataIndex: "investment",
-        key: "investment",
+        dataIndex: "omr",
+        key: "omr",
         editable: false,
         children: [
-            { title: "Recurring", dataIndex: "use", key: "use", editable: false },
-            { title: "Non-Recurring", dataIndex: "disposal", key: "disposal", editable: false }
+            { title: "Recurring", dataIndex: "recurring", key: "recurring", editable: false },
+            { title: "Non-Recurring", dataIndex: "non-recurring", key: "non-recurring", editable: false }
         ]
     },
     {
         title: "Replace",
-        dataIndex: "investment",
-        key: "investment",
+        dataIndex: "replace",
+        key: "replace",
         editable: false
     },
     {
         title: "RV",
-        dataIndex: "investment",
-        key: "investment",
+        dataIndex: "rv",
+        key: "rv",
         editable: false
     },
     {
@@ -100,24 +78,67 @@ const NPVByAltTableCols = [
     }
 ];
 
+const [useData] = bind(data$, {});
+
+const required$ = data$.pipe(map((datas) => datas?.required));
+const resultsCols$ = required$.pipe(
+    map((alts) => {
+        const cols = alts.map((alt) => {
+            return {
+                title: ` Alt #${alt?.altId.toString()}`,
+                dataIndex: alt?.altId.toString(),
+                key: alt?.altId.toString(),
+                editable: false
+            };
+        });
+        cols.unshift({
+            title: "Year",
+            dataIndex: "year",
+            key: "year",
+            editable: false
+        });
+        return cols;
+    })
+);
+
+const resultsAlternatives$ = required$.pipe(map((alts) => alts.map((alt) => alt.altId)));
+
+const [useResultsAlternatives] = bind(resultsAlternatives$, []);
+const [useResultsColumns] = bind(resultsCols$, []);
+
+const { change$: AlternativeChange$, component: AlternativeDropdown } = dropdown(Object.values(alts));
+
 // {json.optional[0].altId}
 
 export default function AnnualResults() {
+    const data = useData();
+    console.log(data);
+
+    // [
+    //     year:..,
+    //     alt1:...,
+    //     alt2...,
+    // ]
+
     return (
-        <div className={"w-full h-full "}>
-            <h1>Annual Results for Alternative</h1>
-            <AlternativeDropdown className={"w-1/2"} label="Annual Results for Alternative" />
+        <div className={"w-full h-full p-5 "}>
             <div className="grid grid-cols-2 gap-4">
                 <div>
                     <Title level={5}>NPV Cash Flow Comparison</Title>
                     <Divider />
-                    <NPVComparisonTable columns={NPVComparisonTableCols} />
+                    <NPVComparisonTable columns={useResultsColumns()} />
                 </div>
                 <div>
                     <Title level={5}>NPV Cash Flows</Title>
                     <Divider />
                 </div>
             </div>
+            <br />
+            <div>
+                <h1>Annual Results for Alternative</h1>
+                <AlternativeDropdown className={"w-1/4"} label="Annual Results for Alternative" />
+            </div>
+            <br />
             <Title level={5}>NPV Cash Flow by Alternative</Title>
             <Divider />
             <NPVByAltTable columns={NPVByAltTableCols} />
