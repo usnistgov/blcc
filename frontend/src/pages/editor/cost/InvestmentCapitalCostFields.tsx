89b80c9407a19316928c5462874c81eda9eb94fd
Luke Donmoyer <luke.donmoyer@nist.gov>
2023-12-19 15:44:02 -0500

    Adding fields

--- frontend/src/pages/editor/cost/InvestmentCapitalCostFields.tsx
+++ frontend/src/pages/editor/cost/InvestmentCapitalCostFields.tsx
@@ -1,3 +1,21 @@
+import numberInput from "../../../components/InputNumber";
+
+const { component: InitialCostInput } = numberInput();
+const { component: AnnualRateOfChangeInput } = numberInput();
+const { component: ExpectedLifeInput } = numberInput();
+const { component: CostAdjustmentFactorInput } = numberInput();
+
 export default function InvestmentCapitalCostFields() {
-    return <div className={"flex flex-col"}>Investment Capital Cost Fields</div>;
+    return (
+        <div className={"flex flex-col"}>
+            <div className={"flex flex-row"}>
+                <InitialCostInput label={"Initial Cost (Base Year Dollars)"} before={"$"} controls />
+                <AnnualRateOfChangeInput label={"Annual Rate of Change"} after={"%"} controls />
+                <ExpectedLifeInput label={"Expected Lifetime"} after={"years"} controls />
+            </div>
+            <div className={"flex flex-row"}>
+                <CostAdjustmentFactorInput label={"Cost Adjustment Factor"} after={"%"} controls />
+            </div>
+        </div>
+    );
 }
--- frontend/src/pages/editor/cost/WaterCostFields.tsx
+++ frontend/src/pages/editor/cost/WaterCostFields.tsx
@@ -1,3 +1,14 @@
+import dropdown from "../../../components/Dropdown";
+import { LiquidUnit } from "../../../blcc-format/Format";
+
+const { component: UnitDropdown } = dropdown(Object.values(LiquidUnit));
+
 export default function WaterCostFields() {
-    return <div className={"flex flex-col"}>Water Cost Fields</div>;
+    return (
+        <div className={"flex flex-col"}>
+            <div className={"flex flex-row"}>
+                <UnitDropdown label={"Unit"} />
+            </div>
+        </div>
+    );
 }
