1959a17dbb8212208f3759982ed76357ea5298ab
purav-parekh <purav98@gmail.com>
2023-12-20 15:14:59 -0500

    show data based on selected alternative

--- frontend/src/pages/results/AlternativeResults.tsx
+++ frontend/src/pages/results/AlternativeResults.tsx
@@ -1,6 +1,6 @@
 import { bind } from "@react-rxjs/core";
 import { Divider, Typography } from "antd";
-import { of } from "rxjs";
+import { combineLatest, of } from "rxjs";
 import { map } from "rxjs/operators";
 import { json } from "../../../../docs/FederalFinancedE3Result";
 import dropdown from "../../components/Dropdown";
@@ -29,55 +29,80 @@ type colsType = {
     width?: string;
 };
 
-const resultsCols$ = required$.pipe(
+const resultsAlternatives$ = required$.pipe(map((alts) => alts.map((alt) => alt.altId)));
+const { change$: AlternativeChange$, component: AlternativeDropdown } = dropdown(resultsAlternatives$);
+
+const resultsCols$ = AlternativeChange$.pipe(
     map((alts) => {
-        const cols: colsType[] = alts.map((alt) => {
-            return {
-                title: ` Alt #${alt?.altId.toString()}`,
-                dataIndex: alt?.altId.toString(),
-                key: alt?.altId.toString(),
+        const cols: colsType[] = [
+            {
+                title: "Year",
+                dataIndex: "year",
+                key: "year",
+                editable: false,
+                fixed: true,
+                width: "75px"
+            },
+            {
+                title: ` Alt #${alts.toString()}`,
+                dataIndex: alts.toString(),
+                key: alts.toString(),
                 editable: false,
                 fixed: false,
                 align: "right"
-            };
-        });
-        cols.unshift({
-            title: "Year",
-            dataIndex: "year",
-            key: "year",
-            editable: false,
-            fixed: true,
-            width: "75px"
-        });
+            }
+        ];
         return cols;
     })
 );
-
-const resultsAlternatives$ = required$.pipe(map((alts) => alts.map((alt) => alt.altId)));
 
 const [useResultsAlternatives] = bind(resultsAlternatives$, []);
 const [useResultsColumns] = bind(resultsCols$, []);
 
 const cashFlow$ = required$.pipe(map((r) => r.map((a) => a.totalCostsDiscounted)));
-const NPVComparisonData$ = cashFlow$.pipe(
+// const NPV$ = cashFlow$.pipe(
+//     map((cash) => {
+//         const cols = [];
+//         for (let i = 0; i < cash?.[0]?.length; i++) {
+//             const subCols = [];
+//             subCols.push(i);
+//             for (let j = 0; j < cash.length; j++) {
+//                 subCols.push(dollarFormatter.format(cash[j][i]));
+//             }
+//             cols.push(subCols);
+//         }
+//         // console.log(cols);
+//         const columnData = cols.map((arr) => {
+//             const key = arr[0].toString();
+//             const year = arr[0];
+//             const values = arr.slice(1);
+//             console.log(values, { key, year, ...values });
+//             return { key, year, ...values };
+//         });
+//         return columnData;
+//     })
+// );
+
+const NPVComparisonData$ = combineLatest([AlternativeChange$, cashFlow$]).pipe(
     map((cash) => {
+        const alt = cash[0];
+        const data = cash[1][alt];
         const cols = [];
-        for (let i = 0; i < cash?.[0]?.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             const subCols = [];
             subCols.push(i);
-            for (let j = 0; j < cash.length; j++) {
-                subCols.push(dollarFormatter.format(cash[j][i]));
+            for (let j = 0; j < cash.length - 1; j++) {
+                subCols.push(dollarFormatter.format(data[i]));
             }
             cols.push(subCols);
         }
-
         const columnData = cols.map((arr) => {
-            const key = arr[0].toString();
-            const year = arr[0];
-            const values = arr.slice(1);
-            return { key, year, ...values };
+            const obj = {};
+            obj["key"] = arr[0].toString();
+            obj["year"] = arr[0];
+            obj[alt] = arr[1];
+            return obj;
         });
-
         return columnData;
     })
 );
@@ -113,7 +138,6 @@ const cashFlowData$ = cashFlow$.pipe(
     })
 );
 
-const { change$: AlternativeChange$, component: AlternativeDropdown } = dropdown(resultsAlternatives$);
 const { component: NPVComparisonTable } = table(NPVComparisonData$);
 const { component: ResourcesTable } = table(cashFlowData$);
 
@@ -155,7 +179,7 @@ export default function AlternativeResults() {
     return (
         <div className={"w-full h-full p-5 "}>
             <div>
-                <AlternativeDropdown className={"w-1/4"} label="Alternative Results" />
+                <AlternativeDropdown className={"w-1/4"} label="Alternative Results" placeholder="Select Alternative" />
             </div>
             <br />
             <div className="grid grid-cols-2 gap-4">
