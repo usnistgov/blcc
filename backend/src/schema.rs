9678fb4423eb992bb5be884c4891cbc3ec158e66
Luke Donmoyer <lukedonmoyer@gmail.com>
2024-01-10 14:44:29 -0500

    Added escalation rates endpoint

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,9 +1,21 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    escalation_rates (year) {
+        year -> Int4,
+        rate -> Nullable<Float8>,
+    }
+}
+
+diesel::table! {
     zip_state (zipcode) {
         zipcode -> Int4,
         #[max_length = 2]
         state -> Varchar,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    escalation_rates,
+    zip_state,
+);
