2911611e2adf860f3d5301f0882c18ee4288e510
Luke Donmoyer <luke.donmoyer@nist.gov>
2024-01-10 10:11:26 -0500

    Added connection pool

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,19 +1,12 @@
-use std::sync::Mutex;
 use actix_web::{get, Responder};
 use actix_web::web::{Data, Json, scope, ServiceConfig};
-use diesel::{ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl, SelectableHelper};
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper};
 use serde::Deserialize;
-use diesel::prelude::*;
-use crate::AppState;
 
+use crate::DbPool;
 use crate::models::*;
 use crate::schema::zip_state::dsl::*;
 use crate::schema::zip_state::zipcode;
-
-#[get("/test")]
-async fn test_handler() -> impl Responder {
-    Json("Hello, World!")
-}
 
 #[derive(Deserialize)]
 struct ZipStateRequest {
@@ -21,25 +14,24 @@ struct ZipStateRequest {
 }
 
 #[get("/zip-state")]
-async fn get_state_from_zip(request: Json<ZipStateRequest>, data: Data<Mutex<AppState>>) -> impl Responder {
+async fn get_state_from_zip(request: Json<ZipStateRequest>, data: Data<DbPool>) -> impl Responder {
     let zip = request.zip;
-    let mut db = data.lock().unwrap();
+    let mut db = data.get().expect("Failed to get a connection");
 
-    zip_state
-        .filter(zipcode.eq(zip))
-        .limit(1)
-        .select(ZipState::as_select())
-        .load(&mut db.db_connection)
-        .expect(&*format!("Could not find state for zipcode {}", zip))
-        .first()
-        .unwrap()
-        .state
+    Json(
+        zip_state
+            .filter(zipcode.eq(zip))
+            .limit(1)
+            .select(ZipState::as_select())
+            .first(&mut db)
+            .expect(&*format!("Could not find state for zipcode {}", zip))
+            .state
+    )
 }
 
 pub fn config_api(config: &mut ServiceConfig) {
     config.service(
         scope("/api")
-            .service(test_handler)
             .service(get_state_from_zip)
     );
 }
