0cbd93c685bd077e35695833b90bc4454e70a1c7
Luke Donmoyer <luke.donmoyer@nist.gov>
2024-05-28 09:10:17 -0400

    Working on building frontend

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -2,10 +2,25 @@
     description = "BLCC react frontend SPA";
 
     inputs = {
-
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        pnpm2nix.url = "github:LukeDonmoyer/pnpm2nix-nzbr?ref=version-9";
     };
 
-    outputs = {}: {
+    outputs = {self, nixpkgs, flake-utils, pnpm2nix}:
+        flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+        let
+            pkgs = import nixpkgs {
+                inherit system;
+            };
 
-    };
+            blcc-frontend = pnpm2nix.packages.${system}.mkPnpmPackage {
+                src = ./.;
+            };
+        in {
+            packages = {
+                inherit blcc-frontend;
+                default = blcc-frontend;
+            };
+        });
 }
