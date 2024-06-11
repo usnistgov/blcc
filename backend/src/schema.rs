2a682d2c690db2cd6b971d4a42b93717895134bd
Luke Donmoyer <lukedonmoyer@gmail.com>
2024-06-11 10:10:14 -0400

    Added energy_prices table

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,6 +1,22 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    energy_prices (release_year, year, division, sector) {
+        release_year -> Int4,
+        year -> Int4,
+        division -> Text,
+        sector -> Text,
+        case -> Text,
+        region -> Text,
+        propane -> Nullable<Float8>,
+        distillate_fuel_oil -> Nullable<Float8>,
+        residual_fuel_oil -> Nullable<Float8>,
+        natural_gas -> Nullable<Float8>,
+        electricity -> Nullable<Float8>,
+    }
+}
+
+diesel::table! {
     escalation_rates (release_year, year, division, sector) {
         release_year -> Int4,
         year -> Int4,
@@ -60,6 +76,7 @@ diesel::table! {
 }
 
 diesel::allow_tables_to_appear_in_same_query!(
+    energy_prices,
     escalation_rates,
     region_case_ba,
     scc,
