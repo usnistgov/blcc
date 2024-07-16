7a753b0ca6f743dcf7d019a7a23d1c6c308a140f
Luke Donmoyer <luke.donmoyer@nist.gov>
2024-07-16 09:19:30 -0400

    Adding other costs

--- frontend/src/pages/editor/cost/OtherCostFields.tsx
+++ frontend/src/pages/editor/cost/OtherCostFields.tsx
@@ -1,7 +1,23 @@
+import { Select } from "antd";
+import Title from "antd/es/typography/Title";
+import Recurring from "components/Recurring";
+import { NumberInput } from "components/input/InputNumber";
+import TextInput, { TextInputType } from "components/input/TextInput";
+import { EMPTY, Subject } from "rxjs";
+
 export default function OtherCostFields() {
     return (
         <div className={"max-w-screen-lg p-6"}>
-            <div className={"grid grid-cols-2 gap-x-16 gap-y-4"}>Other Cost Fields</div>
+            <div className={"grid grid-cols-2 gap-x-16 gap-y-4"}>
+                <div>
+                    <Title level={5}>{"Tags"}</Title>
+                    <Select className={"w-full"} mode={"tags"} onChange={console.log} />
+                </div>
+                <NumberInput className={"w-full"} label={"Initial Occurrence"} value$={EMPTY} />
+                <TextInput className={"w-full"} label={"Unit Name"} type={TextInputType.PRIMARY} wire={new Subject()} />
+                <NumberInput className={"w-full"} label={"Number of Units"} value$={EMPTY} />
+                <Recurring />
+            </div>
         </div>
     );
 }
--- frontend/src/pages/editor/cost/OtherNonMonetaryCostFields.tsx
+++ frontend/src/pages/editor/cost/OtherNonMonetaryCostFields.tsx
@@ -1,7 +1,17 @@
+import { EMPTY, Subject } from "rxjs";
+import Recurring from "../../../components/Recurring";
+import { NumberInput } from "../../../components/input/InputNumber";
+import TextInput, { TextInputType } from "../../../components/input/TextInput";
+
 export default function OtherNonMonetaryCostFields() {
     return (
         <div className={"max-w-screen-lg p-6"}>
-            <div className={"grid grid-cols-2 gap-x-16 gap-y-4"}>Other Non-monetary Cost Fields</div>{" "}
+            <div className={"grid grid-cols-2 gap-x-16 gap-y-4"}>
+                <NumberInput className={"w-full"} label={"Initial Occurrence"} value$={EMPTY} />
+                <TextInput className={"w-full"} label={"Unit Name"} type={TextInputType.PRIMARY} wire={new Subject()} />
+                <NumberInput className={"w-full"} label={"Number of Units"} value$={EMPTY} />
+                <Recurring />
+            </div>
         </div>
     );
 }
