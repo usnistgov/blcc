3ddb75d43e26c8e4e1cdd8fafa412bd2e0cab96f
Luke Donmoyer <lukedonmoyer@gmail.com>
2024-07-18 11:58:11 -0400

    Use StrictMode for development

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,5 +2,10 @@ import App from "App";
 import ReactDOM from "react-dom/client";
 import "index.css";
 import "ant-overridden.css";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
