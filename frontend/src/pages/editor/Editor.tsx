45b4bdba9548bd986988c72f1f1a665d2f6eb0bb
Luke Donmoyer <lukedonmoyer@gmail.com>
2025-01-28 11:05:08 -0500

    Remove animate precense from cost navigation which was causing errors

--- frontend/src/pages/editor/Editor.tsx
+++ frontend/src/pages/editor/Editor.tsx
@@ -15,6 +15,7 @@ import AlternativeSummary from "pages/editor/AlternativeSummary";
 import Cost from "pages/editor/Cost";
 import Alternatives from "pages/editor/alternative/Alternatives";
 import GeneralInformation from "pages/editor/general_information/GeneralInformation";
+import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 
 Effect.runPromise(
@@ -42,16 +43,16 @@ export default function Editor() {
             <EditorAppBar />
 
             <div className={"flex h-full overflow-hidden"}>
+                <Routes location={location}>
+                    <Route path={"*"} element={<Navigation />} />
+                </Routes>
+
                 <AnimatePresence mode={"wait"}>
                     <Routes
                         location={location}
-                        //key={location.pathname.startsWith("/editor/alternative/") ? "cost-navigation" : "navigation"}
+                        key={location.pathname.startsWith("/editor/alternative/") ? "cost-navigation" : "navigation"}
                     >
-                        <Route path={"*"} element={<Navigation />}>
-                            <Route path={"alternative/:alternativeID"}>
-                                <Route index path={"*"} element={<CostNavigation />} />
-                            </Route>
-                        </Route>
+                        <Route path={"alternative/:alternativeID/*"} element={<CostNavigation />} />
                     </Routes>
                 </AnimatePresence>
 
