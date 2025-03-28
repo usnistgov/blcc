5e34d5d53580093bab1250cfe4c07298e50d4ac9
vsauder-NIST <victoria.sauder@nist.gov>
2025-03-28 16:48:52 -0400

    Revert "See if different pathname will fix logo fetching"
    This reverts commit 96fd71c2bb66d8fd96fc62d000243f26201fa689.

--- frontend/src/components/Pdf.tsx
+++ frontend/src/components/Pdf.tsx
@@ -1,10 +1,5 @@
 import { Document, Image, Page, Text, View } from "@react-pdf/renderer";
-import type {
-	AltResults,
-	Annual,
-	GraphSources,
-	Summary,
-} from "blcc-format/ExportTypes";
+import type { AltResults, Annual, GraphSources, Summary } from "blcc-format/ExportTypes";
 import type { Alternative, Cost, Project } from "blcc-format/Format";
 import Alternatives from "components/pdf/Alternatives";
 import GeneralInformation from "components/pdf/GeneralInformation";
@@ -15,60 +10,46 @@ import Results from "components/pdf/Results";
 import { styles } from "components/pdf/pdfStyles";
 
 type PdfProps = {
-	project: Project;
-	alternatives: Alternative[];
-	costs: Cost[];
-	summary: Summary;
-	annual: Annual;
-	altResults: AltResults;
-	graphSources: GraphSources;
+    project: Project;
+    alternatives: Alternative[];
+    costs: Cost[];
+    summary: Summary;
+    annual: Annual;
+    altResults: AltResults;
+    graphSources: GraphSources;
 };
 
-export default function Pdf({
-	project,
-	alternatives,
-	costs,
-	summary,
-	annual,
-	altResults,
-	graphSources,
-}: PdfProps) {
-	return (
-		<Document>
-			<Page style={styles.page} size="LETTER">
-				<NISTHeader />
-				<View style={styles.blccHeader}>
-					<Image style={styles.logo} src="images/logo.png" />
-					<Text style={styles.date}>
-						Report Generated:{" "}
-						{`${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`}
-					</Text>
-				</View>
-				<GeneralInformation project={project} />
-				<PageNumber />
-			</Page>
-			<Page size="LETTER">
-				<NISTHeader />
-				<Alternatives
-					alts={alternatives}
-					costs={costs}
-					releaseYear={project.releaseYear}
-					project={project}
-				/>
-				<PageNumber />
-			</Page>
-			<Page size="LETTER">
-				<NISTHeader />
-				<Results
-					alternatives={alternatives}
-					summary={summary}
-					annual={annual}
-					altResults={altResults}
-					graphs={graphSources}
-				/>
-				<PdfDisclaimer />
-				<PageNumber />
-			</Page>
-		</Document>
-	);
+export default function Pdf({ project, alternatives, costs, summary, annual, altResults, graphSources }: PdfProps) {
+    return (
+        <Document>
+            <Page style={styles.page} size="LETTER">
+                <NISTHeader />
+                <View style={styles.blccHeader}>
+                    <Image style={styles.logo} src="../public/logo.png" />
+                    <Text style={styles.date}>
+                        Report Generated: {`${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`}
+                    </Text>
+                </View>
+                <GeneralInformation project={project} />
+                <PageNumber />
+            </Page>
+            <Page size="LETTER">
+                <NISTHeader />
+                <Alternatives alts={alternatives} costs={costs} releaseYear={project.releaseYear} project={project} />
+                <PageNumber />
+            </Page>
+            <Page size="LETTER">
+                <NISTHeader />
+                <Results
+                    alternatives={alternatives}
+                    summary={summary}
+                    annual={annual}
+                    altResults={altResults}
+                    graphs={graphSources}
+                />
+                <PdfDisclaimer />
+                <PageNumber />
+            </Page>
+        </Document>
+    );
 }
